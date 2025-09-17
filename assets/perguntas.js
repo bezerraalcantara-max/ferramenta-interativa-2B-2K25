faccf52ce9801c0d243639a4bef5d5080687efde
bezerraalcantara-max <bezerra.alcantara@escola.pr.gov.br>
2025-09-17 16:25:10 -0300

    8h4t6

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -2,3 +2,8 @@ criacartao()
 'Programaçao'
 'O que é javaScript?'
 'O JavaScript é uma linguagem de programação'
+
+criacartao()
+'taxa selic'
+'o que significa a sigla selic?'
+'sistema de liquidação e custodia'
